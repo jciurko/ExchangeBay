3719df00864ed302c39d3c34efc5623d0ce61c3f
Benjamin Grant <grantb3@coventry.ac.uk>
2019-11-18 14:32:15 +0000

    Properly tag constructor in user.js
    Unsure this makes a huge difference, however it is best practise after reviewing lecture slides of Week 5

--- modules/user.js
+++ modules/user.js
@@ -14,6 +14,7 @@ class User {
 
 	/**
      * Initialises database and adds 'users' table if it does not already exist
+     * @constructor
      * @param {String} [dbName] - The name of the database. Defaults to :memory:
      * @returns {User} New instance of User class
      */
