ec9ddca07858e28edf05ecc3c1c2eda4a8aeecbc
Benjamin Grant <grantb3@coventry.ac.uk>
2019-11-20 18:56:44 +0000

    Add unit tests for Listing create() function
    As part of TDD methodology I have written the unit tests for this function before implementation.

--- unit tests/listing.spec.js
+++ unit tests/listing.spec.js
@@ -33,7 +33,7 @@ describe('getMetadata()', () => {
 		done()
 	})
 
-	test('error if non-numeric listing_id', async done => {
+	test('error with non-numeric listing_id', async done => {
 		expect.assertions(1)
 		const listing = await new Listing('exchangebay.db')
 		await expect( listing.getMetadata('test') )
@@ -43,13 +43,13 @@ describe('getMetadata()', () => {
 
 })
 
-/*describe('create()', () => {
+describe('create()', () => {
 
-	test('get data from a valid listing', async done => {
+	test('create listing with valid info', async done => {
 		expect.assertions(5)
-		const listing = await new Listing()
-		const data = await listing.getMetadata(1)
-		console.log(data)
+		const listing = await new Listing('exchangebay.db')
+		const listing_id = await listing.create(1, 'item_name', 'item_description', 'img_location')
+		const data = await listing.getMetadata(listing_id)
 		expect(data).toHaveProperty('itemname')
 		expect(data).toHaveProperty('itemdescription')
 		expect(data).toHaveProperty('imgloc')
@@ -58,28 +58,29 @@ describe('getMetadata()', () => {
 		done()
 	})
 
-	test('get data from an invalid listing', async done => {
+	test('create listing with no item_name', async done => {
 		expect.assertions(1)
-		const listing = await new Listing()
+		const listing = await new Listing('exchangebay.db')
+		await expect( listing.create(1, '', 'item_description', 'img_location') )
+			.rejects.toEqual( Error('no item_name provided') )
+		done()
+	})
+
+	test('create listing with no item_description', async done => {
+		expect.assertions(1)
+		const listing = await new Listing('exchangebay.db')
+		const listing_id = await listing.create(1, 'item_name', '', 'img_location')
 		await expect( listing.getMetadata(-1) )
-			.rejects.toEqual( Error('listing with ID "-1" not found') )
+			.rejects.toEqual( Error('no item_description provided') )
 		done()
 	})
 
-	test('error if blank listing_id', async done => {
+	test('create listing with no img_location', async done => {
 		expect.assertions(1)
-		const listing = await new Listing()
-		await expect( listing.getMetadata('') )
-			.rejects.toEqual( Error('listing with ID "" not found') )
+		const listing = await new Listing('exchangebay.db')
+		await expect( listing.create(1, 'item_name', 'item_description', '') )
+			.rejects.toEqual( Error('no img_location provided') )
 		done()
 	})
 
-	test('error if non-numeric listing_id', async done => {
-		expect.assertions(1)
-		const listing = await new Listing()
-		await expect( listing.getMetadata('test') )
-			.rejects.toEqual( Error('listing with ID "test" not found') )
-		done()
-	})
-
-})*/
+})
