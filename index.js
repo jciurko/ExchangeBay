ab1db1bbe594281d5057a051f5b6fcd9ab49499c
Lewis Stokes <stokesl4@coventry.ac.uk>
2019-11-25 13:49:45 +0000

    Fixed basic errors in index.js
    Fixed basic styling and errors in linter.
    
    Still some errors due to unused variables

--- index.js
+++ index.js
@@ -5,37 +5,42 @@
 'use strict'
 
 /* MODULE IMPORTS */
+
+// -- UNUSED MODULE -- \\
 const bcrypt = require('bcrypt-promise')
-const Koa = require('koa')
-const Router = require('koa-router')
-const views = require('koa-views')
-const staticDir = require('koa-static')
-const bodyParser = require('koa-bodyparser')
-const koaBody = require('koa-body')({multipart: true, uploadDir: '.'})
-const session = require('koa-session')
-const sqlite = require('sqlite-async')
-const fs = require('fs-extra')
-const mime = require('mime-types')
+
+const Koa = require('koa');
+const Router = require('koa-router');
+const views = require('koa-views');
+const staticDir = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body')({multipart: true, uploadDir: '.'});
+const session = require('koa-session');
+
+// -- Declared but never used -- \\
+const sqlite = require('sqlite-async');
+const fs = require('fs-extra');
+const mime = require('mime-types');
 //const jimp = require('jimp')
 
 /* IMPORT CUSTOM MODULES */
-const User = require('./modules/user')
-const Listing = require('./modules/listing')
+const User = require('./modules/user');
+const Listing = require('./modules/listing');
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
 /* CONFIGURING THE MIDDLEWARE */
 app.keys = ['darkSecret']
-app.use(staticDir('public'))
-app.use(bodyParser())
-app.use(session(app))
-app.use(views(`${__dirname}/views`, { extension: 'handlebars' }, {map: { handlebars: 'handlebars' }}))
+app.use(staticDir('public'));
+app.use(bodyParser());
+app.use(session(app));
+app.use(views(`${__dirname}/views`, { extension: 'handlebars' }, {map: { handlebars: 'handlebars' }}));
 
-const defaultPort = 8080
-const port = process.env.PORT || defaultPort
-const dbName = 'exchangebay.db'
-const saltRounds = 10
+const defaultPort = 8080;
+const port = process.env.PORT || defaultPort;
+const dbName = 'exchangebay.db';
+const saltRounds = 10;
 
 /**
  * The home page.
@@ -45,11 +50,11 @@ const saltRounds = 10
  */
 router.get('/', async ctx => {
 	try {
-		const listing = await new Listing(dbName)
-        let listings = await listing.getListings()
-        await ctx.render('homepage', {listings: listings})
+		const listing = await new Listing(dbName);
+		let listings = await listing.getListings();
+		await ctx.render('homepage', {listings: listings});
 	} catch(err) {
-		await ctx.render('homepage', {listings: []})
+		await ctx.render('homepage', {listings: []});
 	}
 })
 
@@ -59,7 +64,7 @@ router.get('/', async ctx => {
  * @name Register Page
  * @route {GET} /register
  */
-router.get('/register', async ctx => await ctx.render('register'))
+router.get('/register', async ctx => await ctx.render('register'));
 
 /**
  * The about page.
@@ -67,7 +72,7 @@ router.get('/register', async ctx => await ctx.render('register'))
  * @name About Page
  * @route {GET} /about
  */
-router.get('/about', async ctx => await ctx.render('about'))
+router.get('/about', async ctx => await ctx.render('about'));
 
 /**
  * The listing detail page.
@@ -76,16 +81,16 @@ router.get('/about', async ctx => await ctx.render('about'))
  * @route {GET} /item/{id}
  */
 router.get('/item/:id', async ctx => {
-    // call the functions in the listing module
-    const listing = await new Listing(dbName)
+	// call the functions in the listing module
+	const listing = await new Listing(dbName);
 
-    const parameters = ctx.params
-    try{
-        const data = await listing.getMetadata(parameters.id)
-        await ctx.render('listing', data)
-    }catch(err){
-        await ctx.render('homepage', {message: err.message})
-    }
+	const parameters = ctx.params;
+	try{
+		const data = await listing.getMetadata(parameters.id);
+		await ctx.render('listing', data);
+	}catch(err){
+		await ctx.render('homepage', {message: err.message});
+	}
     
     
 })
@@ -98,21 +103,21 @@ router.get('/item/:id', async ctx => {
  * @route {POST} /register
  */
 router.post('/register', koaBody, async ctx => {
-    try {
-        // extract the data from the request
-        const body = ctx.request.body
-        console.log(body)
-        const {path, type} = ctx.request.files.avatar
-        // call the functions in the module
-        const user = await new User(dbName)
-        await user.register(body.user, body.pass, body.forename, body.surname, body.email)
-        // await user.uploadPicture(path, type)
-        // redirect to the home page
-        ctx.redirect(`/?msg=new user "${body.name}" added`)
-    } catch(err) {
-        await ctx.render('error', {message: err.message})
-    }
-})
+	try {
+		// extract the data from the request
+		const body = ctx.request.body;
+		console.log(body);
+		const {path, type} = ctx.request.files.avatar;
+		// call the functions in the module
+		const user = await new User(dbName);
+		await user.register(body.user, body.pass, body.forename, body.surname, body.email);
+		// await user.uploadPicture(path, type)
+		// redirect to the home page
+		ctx.redirect(`/?msg=new user "${body.name}" added`);
+	} catch(err) {
+		await ctx.render('error', {message: err.message});
+	}
+});
 
 /**
  * The login page.
@@ -121,11 +126,11 @@ router.post('/register', koaBody, async ctx => {
  * @route {GET} /login
  */
 router.get('/login', async ctx => {
-    const data = {}
-    if(ctx.query.msg) data.msg = ctx.query.msg
-    if(ctx.query.user) data.user = ctx.query.user
-    await ctx.render('login', data)
-})
+	const data = {};
+	if(ctx.query.msg) data.msg = ctx.query.msg;
+	if(ctx.query.user) data.user = ctx.query.user;
+	await ctx.render('login', data);
+});
 
 /**
  * The script to process user logins.
@@ -134,16 +139,16 @@ router.get('/login', async ctx => {
  * @route {POST} /login
  */
 router.post('/login', async ctx => {
-    try {
-        const body = ctx.request.body
-        const user = await new User(dbName)
-        await user.login(body.user, body.pass)
-        ctx.session.authorised = true
-        return ctx.redirect('/?msg=you are now logged in...')
-    } catch(err) {
-        await ctx.render('error', {message: err.message})
-    }
-})
+	try {
+		const body = ctx.request.body;
+		const user = await new User(dbName);
+		await user.login(body.user, body.pass);
+		ctx.session.authorised = true;
+		return ctx.redirect('/?msg=you are now logged in...');
+	} catch(err) {
+		await ctx.render('error', {message: err.message});
+	}
+});
 
 /**
  * The logout page/script.
@@ -152,9 +157,9 @@ router.post('/login', async ctx => {
  * @route {GET} /logout
  */
 router.get('/logout', async ctx => {
-    ctx.session.authorised = null
-    ctx.redirect('/?msg=you are now logged out')
+	ctx.session.authorised = null;
+	ctx.redirect('/?msg=you are now logged out');
 })
 
 app.use(router.routes())
-module.exports = app.listen(port, async() => console.log(`listening on port ${port}`))
+module.exports = app.listen(port, async() => console.log(`listening on port ${port}`));
